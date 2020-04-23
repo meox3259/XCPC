5f8c4f4a58a21361eb7cb0fe710869aeecc25c00
meox3259 <meox1223@gmail.com>
2020-04-23 15:03:05 +0800

    2020/4/23

---
+++ Code/Nanjing2019H.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	if(a == 1 && b == 0 && c == 0) {
+		cout << "YES" << '\n';
+		cout << 0 << '\n';
+		return 0;
+	}
+
+	if(a <= b + c) {
+		cout << "NO" << '\n';
+	} else {
+		cout << "YES" << '\n';
+		cout << 2 * (b + c) + 1 << '\n';
+	}
+
+	return 0;
+}
