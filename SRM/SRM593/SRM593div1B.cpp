7c26197b9200f42f1bf96a4d385817c53d5c7e71
meox3259 <meox1223@gmail.com>
2020-05-07 20:36:01 +0800

    2020/5/7

--- SRM/SRM593/SRM593div1B.cpp
+++ SRM/SRM593/SRM593div1B.cpp
@@ -7,23 +7,33 @@ class MayTheBestPetWin {
     int calc(vector <int> A, vector <int> B) {
         int n = A.size();
         int m = 0;
+		for(int i : A) {
+			m += i;
+		}
         for(int i : B) {
             m += i;
         }
-        vector<vector<int> > dp(n + 1, vector<int> (m + 1));
-        dp[0][0] = 1;
-        int sum = 0;
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j <= sum; ++j) {
-                if(j - A[i] >= 0) {
-                    dp[i + 1][j - A[i]] |= dp[i][j];
-                }
-                dp[i + 1][j + B[i]] |= dp[i][j];
-            }
-            sum += B[i];
-        }
-        for(int i = 0; i <= m; ++i) {
-            
-        }
+		vector<int> dp(m + 1);
+		dp[0] = 1;
+		for(int i = 0; i < n; ++i) {
+			for(int j = m; j >= A[i] + B[i]; --j) {
+				dp[j] |= dp[j - A[i] - B[i]];
+			}
+		}
+		int ans = m;
+		int sa = 0;
+		int sb = 0;
+		for(int i : A) {
+			sa += i;
+		}
+		for(int i : B) {
+			sb += i;
+		}
+		for(int i = 0; i <= m; ++i) {
+			if(dp[i]) {
+				ans = min(ans, max(abs(i - sa), abs(i - sb)));
+			}	
+		}
+		return ans;
     }
 };
